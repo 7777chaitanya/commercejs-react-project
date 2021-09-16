024a77520d4c85b9bf0483b8a0651589b37606bd
chaitanya7666 <chaitu.y25@gmail.com>
2021-09-16 17:14:41 +0530

    modified breakpoints of search component

--- src/components/PopUp/styles.js
+++ src/components/PopUp/styles.js
@@ -1,13 +1,19 @@
 import { makeStyles } from "@material-ui/core";
 
-const useStyles = makeStyles({
-    root : {
-        position : "absolute",
-        left : "30vw",
-        maxHeight : "50vh",
-        overflowY : "auto"
-    }
-
-})
+const useStyles = makeStyles(theme =>({
+  root: {
+    position: "absolute",
+    maxHeight: "50vh",
+    overflowY: "auto",
+    [theme.breakpoints.down("sm")]: {
+      left : "10vw",
+      right : "10vw"
+    },
+    [theme.breakpoints.up("sm")]: {
+        left: "10vw",
+        // right : "30vw"
+    },
+  },
+}));
 
 export default useStyles;
--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,49 +1,30 @@
 import React from "react";
-import { CardHeader, Avatar, IconButton } from '@material-ui/core';
-import { Card } from '@material-ui/core';
-import useStyles from './styles.js';
-import {Link} from "react-router-dom";
+import { CardHeader, Avatar, IconButton } from "@material-ui/core";
+import { Card } from "@material-ui/core";
+import useStyles from "./styles.js";
+import { Link } from "react-router-dom";
 
+const UserCard = ({ item, searchTerm, handleProductClick }) => {
+  const classes = useStyles();
 
-const UserCard = ({item, searchTerm, handleProductClick}) => {
-    const classes = useStyles();
-    
   return (
     <Card className={classes.root}>
-      {(item?.avatarUrl!=="hi") ? (
-      <CardHeader
-        onClick={handleProductClick}
-        component={Link}
-        to={`/products/${item.id}`}
-        avatar={
-          
+      {item?.avatarUrl !== "hi" ? (
+        <CardHeader
+          onClick={handleProductClick}
+          component={Link}
+          to={`/products/${item.id}`}
+          avatar={
             <Avatar
               alt={item?.name}
               src={item?.media?.source}
               className={classes.avatarSize}
             />
-        
-        }
-      
-        title={item.name}
-      />) :
-      (
-        <CardHeader
-        // onClick={handleProductClick}
-        // component={Link}
-        // to={`/products/${item.id}`}
-        // avatar={
-          
-        //     <Avatar
-        //       alt={item?.name}
-        //       src={item?.media?.source}
-        //       className={classes.avatarSize}
-        //     />
-        
-        // }
-      
-        title={item.name}
-      />
+          }
+          title={item.name}
+        />
+      ) : (
+        <CardHeader title={item.name} />
       )}
     </Card>
   );
